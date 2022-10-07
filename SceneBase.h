372a43d9ba5461776d885d408ece85361f4ebbe8
S0624 <fko2247078@stu.o-hara.ac.jp>
2022-10-07 16:02:18 +0900

    Baseクラスの作成

---
+++ SceneBase.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class SceneBase			//ベースクラスの作成
+{
+public:
+	SceneBase(){}
+	virtual ~SceneBase(){}
+
+	virtual void init(){}
+	virtual void end(){}
+
+	virtual void update(){}
+	virtual void draw(){}
+
+	virtual bool isEnd() { return false; }
+private:
+
+};
