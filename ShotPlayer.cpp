71c2cec0d1d881f453525f45a2cffeedec89df41
S0624 <fko2247078@stu.o-hara.ac.jp>
2022-10-13 09:52:36 +0900

    Playerが弾を撃つための処理の追加

--- ShotPlayer.cpp
+++ ShotPlayer.cpp
@@ -1,1 +1,24 @@
+#include"game.h"
 #include"ShotPlayer.h"
+
+namespace
+{
+	constexpr float kShotSpeed = 8.0f;
+}
+
+void ShotPlayer::start(Vec2 pos)
+{
+	ShotBase::start(pos);
+	m_pos.y -= kShotSpeed;
+}
+
+void ShotPlayer::update()
+{
+	if (!m_isExist)return;
+	m_pos += m_vec;
+
+	if (m_pos.y > 0)
+	{
+		m_isExist = false;
+	}
+}
--- ShotPlayer.h
+++ ShotPlayer.h
@@ -4,7 +4,12 @@
 class ShotPlayer :public ShotBase
 {
 public:
+	ShotPlayer(){}
+	~ShotPlayer(){}
 
+	virtual void start(Vec2 pos);		//ショット開始
+
+	virtual void update();				// 更新
 private:
 
 };
