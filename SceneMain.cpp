1588986a4b583b1e5e7d25e64e6b7f89fdf4233b
S0624 <fko2247078@stu.o-hara.ac.jp>
2022-10-13 09:51:39 +0900

    エネミーの表示のための修正

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -1,27 +1,88 @@
 #include"DxLib.h"
 #include "SceneMain.h"
 #include"SceneTitle.h"
+#include"ShotPlayer.h"
+#include<cassert>
+
+
+namespace
+{
+	//ショットの発射間隔
+	constexpr int kShotInterval = 16;
+}
 
 SceneMain::SceneMain()
 {
 	m_isEnd = false;
 }
 
-
-SceneBase* SceneMain::update()
+void SceneMain::init()
 {
+	m_player.init();
+	m_enemy.init();
+}
+
+void SceneMain::end()
+{
+	//m_player.end();
+	for (auto& pShot : m_pShotVt)
+	{
+		assert(pShot);				//バグがあったらわざと止める処理
+		delete pShot;
+		pShot = nullptr;
+	}
+}
+
+void SceneMain::update()
+{
+	m_player.update();
+	m_enemy.update();
+
 	/*int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	if (padState & PAD_INPUT_2)
 	{
-		return (new SceneTitle);
+		m_isEnd = true;
 	}*/
-	return this;
+
+	std::vector<ShotBase*>::iterator it = m_pShotVt.begin();
+	while (it != m_pShotVt.end())
+	{
+		auto& pShot = (*it);
+
+		assert(pShot);				//バグがあったらわざと止める処理
+		pShot->update();
+		if (!pShot->isExsist())
+		{
+			delete pShot;
+			pShot = nullptr;
+
+			//vectorの要素削除
+			it = m_pShotVt.erase(it);		//指定した場所を削除する
+			continue;					//削除したときループの先頭に戻る
+		}
+		it++;
+	}
 }
 
 
 void SceneMain::draw()
 {
-	player.init();
-	player.draw();
+	m_player.draw();
+	m_enemy.draw();
+	
+	for (auto& pShot : m_pShotVt)
+	{
+		if (!pShot) continue;
+		pShot->draw();
+	}
 	DrawString(0, 0, "メイン画面", GetColor(255, 255, 255));
+}
+
+bool SceneMain::createShotPlayer(Vec2 pos)
+{
+	ShotPlayer* pShot = new ShotPlayer;
+	pShot->start(pos);
+	m_pShotVt.push_back(pShot);
+
+	return true;
 }
--- SceneMain.h
+++ SceneMain.h
@@ -1,6 +1,9 @@
 #pragma once
+#include<vector>
 #include"SceneBase.h"
+#include"ShotBase.h"
 #include"Player.h"
+#include"Enemy.h"
 
 class SceneMain : public SceneBase			//ベースクラスを継承してタイトルクラスの作成
 {
@@ -8,15 +11,21 @@ public:
 	SceneMain();
 	virtual ~SceneMain() {}
 
-	virtual void init()override { m_isEnd = false; }
-	virtual void end()override{};
+	void init();
+	void end();
 
-	virtual SceneBase* update()override;
-	virtual void draw()override;
+	void update();
+	void draw();
 
-	virtual bool isEnd()override { return m_isEnd; }
+	bool isEnd() { return m_isEnd; }
+	
+	bool createShotPlayer(Vec2 pos);
+
 private:
 	bool m_isEnd;
 
-	Player player;
+	Player m_player;
+	Enemy m_enemy;
+
+	std::vector< ShotBase*> m_pShotVt;
 };
