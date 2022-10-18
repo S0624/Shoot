1f2f56da9f0727414f5866392d37af88d3b8a3f9
S0624 <fko2247078@stu.o-hara.ac.jp>
2022-10-18 13:55:31 +0900

    デバッグ用に表示していた線の消去

--- ShotBase.cpp
+++ ShotBase.cpp
@@ -1,18 +1,15 @@
 #include"DxLib.h"
 #include"ShotBase.h"
 
-namespace
-{
-	constexpr int kPos_y = 20;
-	constexpr int kRadius = 10;
-}
 
 ShotBase::ShotBase()
 {
 	m_pos.x = 0.0f;
 	m_pos.y = 0.0f;
 	m_vec.y = 0.0f;
-	m_size = 0.0f;
+
+	m_size.x = 0.0f;
+	m_size.y = 40.0f;
 	m_isExist = false;
 }
 
@@ -35,27 +32,5 @@ void ShotBase::update()
 void ShotBase::draw()
 {
 	if (!m_isExist)return;
-	DrawLine(m_pos.x, m_pos.y, m_pos.x, m_pos.y + kPos_y, GetColor(255, 255, 255));    // íeÇï`âÊ
-	//DrawCircle(getCenter().x, getCenter().y, getRadius(), GetColor(0, 255, 255), false);
-	DrawBox(getPos().x -2, getPos().y -2, getPos().x +2, getPos().y + kPos_y +2, GetColor(255, 0, 255), false);			//ìñÇΩÇËîªíË
-
-	DrawFormatString(0, 500, GetColor(0, 255, 0), "íeç¿ïW:%f %f", getPos().x, getPos().y);					//ÉfÉoÉbÉOÇÃÇΩÇﬂÇÃï\é¶
+	DrawLine(m_pos.x, m_pos.y, m_pos.x, m_pos.y + m_size.y, GetColor(255, 255, 255));    // íeÇï`âÊ
 }
-
-
-
-//float ShotBase::getRadius()const
-//{
-//	return kRadius;
-//}
-//Vec2 ShotBase::getCenter()const
-//{
-//	//int sizeX = 0;
-//	//int sizeY = 0;
-//	
-//	//Vec2 result = m_pos;
-//	//result.x += sizeX / 2;
-//	//result.y += sizeY / 2;
-//
-//	return m_pos;
-//}
