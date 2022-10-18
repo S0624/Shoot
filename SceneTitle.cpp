cd4176d6e4614e4a56cb5900d575c5626b87e4ef
S0624 <fko2247078@stu.o-hara.ac.jp>
2022-10-18 10:23:05 +0900

    タイトル画面から変えるシーンの変更

--- SceneTitle.cpp
+++ SceneTitle.cpp
@@ -9,19 +9,18 @@ SceneTitle::SceneTitle()
 
 void SceneTitle::update()
 {
-
-	//int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	//if (padState & PAD_INPUT_1)
-	//{
-	//	//mainに切り替え
-	//	m_isEnd = true;
-	//}
-	
+	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	if (padState & PAD_INPUT_4)
+	{
+		//mainに切り替え
+		m_isEnd = true;
+	}
 }
 
 void SceneTitle::draw()
 {
 	DrawString(620, 380, "タイトル画面", GetColor(255, 255, 255));			//タイトル画面の表示
+	DrawString(620, 480, "Y or KEY Aボタン", GetColor(255, 255, 255));			//タイトル画面の表示
 	
 	DrawBox(0, 0, 25, 25, GetColor(255,255,255), false);    // 四角形を描画
 }
